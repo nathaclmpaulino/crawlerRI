5e9802186f4071331a44086431c6a88d56fb324a
bulgarellim <bulgarelli.mariana1@gmail.com>
2020-10-11 00:16:10 -0300

    code commented

--- tp-crawler/crawler/.ipynb_checkpoints/scheduler-checkpoint.py
+++ tp-crawler/crawler/.ipynb_checkpoints/scheduler-checkpoint.py
@@ -17,7 +17,7 @@ class Scheduler():
                 - `int_page_limit`: Número de páginas a serem coletadas
                 - `int_depth_limit`: Profundidade máxima a ser coletada
                 - `int_page_count`: Quantidade de página já coletada
-                - `dic_url_per_domain`: Fila de URLs por domínio (explicado anteriormente)
+                - `dic_url_per_domain`: Fila de URLs por domínio
                 - `set_discovered_urls`: Conjunto de URLs descobertas, ou seja, que foi extraída em algum HTML e já adicionadas na fila - mesmo se já ela foi retirada da fila. A URL armazenada deve ser uma string.
                 - `dic_robots_per_domain`: Dicionário armazenando, para cada domínio, o objeto representando as regras obtidas no `robots.txt`
         """
@@ -30,7 +30,7 @@ class Scheduler():
         self.set_discovered_urls = set()
         self.dic_robots_per_domain = {}
         
-        for url in arr_urls_seeds:
+        for url in arr_urls_seeds: # cria um url parse para cada posicao do array e adiciona com profundidade de coleta 0 com add_new_page
             urlParse = urlparse(url)
             self.add_new_page(urlParse,0)
 
@@ -58,7 +58,9 @@ class Scheduler():
             e a url não foi descoberta ainda
         """
 
+        # verifica se a página não foi descoberta ainda e se a profundidade dela é menor que a profundidade limite
         if obj_url not in self.set_discovered_urls and int_depth <= self.int_depth_limit:
+            # se as condicoes sao verdadeiras, retorna True
             return True
         else:
             return False
@@ -73,18 +75,20 @@ class Scheduler():
         """
         #https://docs.python.org/3/library/urllib.parse.html
         
+        # cria um objeto Domain com o netloc do Objeto da classe ParseResult com a URL e com o tempo entre requisicoes
         obj_domain = Domain(obj_url.netloc, self.TIME_LIMIT_BETWEEN_REQUESTS)
         
+        # verifica se a pagina pode ser adicionada e retorna false se não puder. Assim, a url nao foi adicionada
         if self.can_add_page(obj_url,int_depth) == False:
             return False
 
-        else:
+        else: # se puder, adiciona o objeto como key do dicionario de urls por dominio coma profundidade de coleta como value
             if (obj_domain in self.dic_url_per_domain):
                 self.dic_url_per_domain[obj_domain].append((obj_url, int_depth))
             else:
                 self.dic_url_per_domain[obj_domain] = [(obj_url, int_depth)]
             
-            self.set_discovered_urls.add(obj_url) # armazenar que este parseresult  já foi descoberto
+            self.set_discovered_urls.add(obj_url) # armazenar que este parseresult já foi descoberto
             return True
 
 
@@ -94,18 +98,19 @@ class Scheduler():
         Obtem uma nova URL por meio da fila. Essa URL é removida da fila.
         Logo após, caso o servidor não tenha mais URLs, o mesmo também é removido.
         """
+        # pega as keys do dicionario de urls
         domains = list(self.dic_url_per_domain.keys())
         
         while (True):
-            for domain in domains:
-                if domain.is_accessible() == True: 
+            for domain in domains: # itera sobre as keys
+                if domain.is_accessible() == True: # verifica se o dominio esta acessivel. Se sim, marque como acessado
                     domain.accessed_now()
-                    if len(self.dic_url_per_domain[domain]) == 0:
+                    if len(self.dic_url_per_domain[domain]) == 0: # verifica se o value do dominio no dicionario esta vazio
                         self.dic_url_per_domain.pop(domain, None)
                     else:
-                        return self.dic_url_per_domain[domain].pop(0) 
+                        return self.dic_url_per_domain[domain].pop(0) # remove url da fila
 
-            time.sleep(self.TIME_LIMIT_BETWEEN_REQUESTS)
+            time.sleep(self.TIME_LIMIT_BETWEEN_REQUESTS) # espera  o tempo entre requisicoes
         
         """
         TIME_LIMIT_BETWEEN_REQUESTS não é a melhor métrica de variável para isso.
