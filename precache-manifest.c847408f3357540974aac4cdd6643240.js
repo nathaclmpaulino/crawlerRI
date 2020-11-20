36e6d385f3f05bbd0d0881bf6796794fe5fd6286
Antônio Sousa <antonioaads@gmail.com>
2020-11-19 23:23:52 -0300

    Adicionado novo build

---
+++ precache-manifest.c847408f3357540974aac4cdd6643240.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9a29c6a2fddb321b6f3e6f769f46c86",
+    "url": "./index.html"
+  },
+  {
+    "revision": "100892179e767a0ed380",
+    "url": "./static/css/main.7b3b8bc7.chunk.css"
+  },
+  {
+    "revision": "f5b0974772551bd2b269",
+    "url": "./static/js/2.96cc03ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.96cc03ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "100892179e767a0ed380",
+    "url": "./static/js/main.b7d38015.chunk.js"
+  },
+  {
+    "revision": "141575565b3272835dd8",
+    "url": "./static/js/runtime-main.1884473d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://nathaclmpaulino.github.io/crawlerRI/precache-manifest.29c9e39c36bbe35700d6ca6e94a87174.js"
+  "./precache-manifest.c847408f3357540974aac4cdd6643240.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://nathaclmpaulino.github.io/crawlerRI/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
