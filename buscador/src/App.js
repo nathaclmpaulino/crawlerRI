65563210f668275879795441ddfae53646dfae55
Antônio Sousa <antonioaads@gmail.com>
2020-11-19 23:24:46 -0300

    Remocao da utilizacao de router para publicacao no ghpages

--- buscador/src/App.js
+++ buscador/src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 import './global.scss'
 
-import Routes from './routes';
+import Main from './pages/Main'
 
 function App() {
   return (
-    <Routes/>
+    <Main/>
   );
 }
 
